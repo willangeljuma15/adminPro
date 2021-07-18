594bc7447dcba6898114a18bd17edc0eb81b69b8
willangeljuma15 <willangeljuma15@gmail.com>
2021-07-18 02:47:36 -0400

    primer commit willangel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+// Modules
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
